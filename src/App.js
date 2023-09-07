78cc171c1815a15e62c647d2ed1ba5fed48e9746
ck <ck@gmail.com>
2023-09-07 15:51:48 +0530

    my first commit to login page

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// import logo from './dora.jfif';
+import './App.css';
+// import { Login } from "./Login";
+// import { Register } from "./Register"; 
+
+function App() {
+  return (
+    <div className="App">
+     <h1>sWETHA</h1>
+    </div>
+  )
+}
+
+export default App;
